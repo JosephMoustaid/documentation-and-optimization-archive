5ea0896df778c8ad6c7947d96f42d70e32f83fa2
YOUSSEF MOUSTAID <125811875+JosephMoustaid@users.noreply.github.com>
2026-07-05 19:23:54 +0100

    Persist previous definition for OPT_LAB_CLONE_1.RETAIL.V_CUSTOMER_LTV_RANKED (exec-2026-06-30T00:01:28Z)

---
+++ OPT_LAB_CLONE_1/executions/exec-2026-06-30T00:01:28Z/previous.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW OPT_LAB_CLONE_1.RETAIL.V_CUSTOMER_LTV_RANKED AS
+SELECT
+ c.customer_id,
+ c.first_name,
+ c.last_name,
+ c.lifetime_value,
+ (SELECT COUNT()
+ FROM customers c2
+ WHERE c2.lifetime_value > c.lifetime_value) + 1 AS ltv_rank
+FROM customers c;
