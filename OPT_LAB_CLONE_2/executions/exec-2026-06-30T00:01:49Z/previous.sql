5adcfdb36092ec86d310f77ed708d9f1ba1ca946
YOUSSEF MOUSTAID <125811875+JosephMoustaid@users.noreply.github.com>
2026-07-06 11:24:59 +0100

    Persist artifacts for exec-2026-06-30T00:01:49Z (previous SQL)

---
+++ OPT_LAB_CLONE_2/executions/exec-2026-06-30T00:01:49Z/previous.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW OPT_LAB_CLONE_2.RETAIL.V_CUSTOMER_ORDER_SUMMARY AS
+SELECT
+ c.,
+ (SELECT COUNT(*) FROM orders o WHERE o.customer_id = c.customer_id) AS num_orders,
+ (SELECT SUM(order_total) FROM orders o WHERE o.customer_id = c.customer_id) AS total_spent,
+ (SELECT MAX(order_date) FROM orders o WHERE o.customer_id = c.customer_id) AS last_order
+FROM customers c;
