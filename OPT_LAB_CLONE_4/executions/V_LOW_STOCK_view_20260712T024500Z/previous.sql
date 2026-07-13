d6ee10eff3dcc3d020bcaafdf0fa3ee5137c735d
YOUSSEF MOUSTAID <125811875+JosephMoustaid@users.noreply.github.com>
2026-07-13 11:37:22 +0100

    Persist execution artifact: previous.sql (OPT_LAB_CLONE_4.RETAIL.V_LOW_STOCK)

---
+++ OPT_LAB_CLONE_4/executions/V_LOW_STOCK_view_20260712T024500Z/previous.sql
@@ -0,0 +1,19 @@
+create or replace view V_LOW_STOCK(
+	INVENTORY_ID,
+	WAREHOUSE_CODE,
+	PRODUCT_ID,
+	QTY_ON_HAND,
+	REORDER_LEVEL,
+	PRODUCT_NAME,
+	SUPPLIER_NAME
+) as
+SELECT
+ i.inventory_id,
+ i.warehouse_code,
+ i.product_id,
+ i.qty_on_hand,
+ i.reorder_level,
+ (SELECT p.product_name FROM products p WHERE p.product_id = i.product_id) AS product_name,
+ (SELECT s.supplier_name FROM suppliers s WHERE s.supplier_id = i.supplier_id) AS supplier_name
+FROM inventory i
+WHERE i.qty_on_hand < i.reorder_level;
